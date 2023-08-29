b9e0df55a96d423ae9100f94f8500f379da78e67
Vlado Latso <vlado@starlogic.net>
2023-08-29 21:30:09 +0200

    0.2.7

---
+++ includes/class-afterorder.php
@@ -0,0 +1,20 @@
+<?php
+namespace MYDAYEMAIL;
+
+class AfterOrder
+{
+
+/*	add_filter('woocommerce_new_order_data', 'action_woocommerce_new_order_data', 10, 2);
+	function action_woocommerce_new_order( $data_arr, $wp_error )
+	{
+		// Here your custom code to filter data
+
+		// Always return the main argument
+		return $data_arr;
+	}*/
+	 	
+	//https://gist.github.com/damiencarbery/dfc45a9ceb52fa336749b363a6e7dd51
+	//https://gist.github.com/lomars/a1f0a17431154846be1a630fc6ee02c9
+	
+}
+?>
